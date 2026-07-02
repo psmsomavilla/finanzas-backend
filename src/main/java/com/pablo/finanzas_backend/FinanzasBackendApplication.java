5472f4fa7432b17a773bf6ee0285f9be2652608a
psmsomavilla <psm.somavilla@gmail.com>
2026-07-02 10:14:30 +0200

    chore: initial Spring Boot project setup with Web, JPA, Security, Lombok dependencies

---
+++ src/main/java/com/pablo/finanzas_backend/FinanzasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.finanzas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzasBackendApplication.class, args);
+	}
+
+}
